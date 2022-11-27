b40ecbc8367dd6e43ce03d9761bd5477c27a3101
Shobhan Sundar Goutam <shobhangoutam@gmail.com>
2022-11-27 23:43:08 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Tours from "./Tours";
+import Loading from "./Loading";
+import axios from "axios";
+
+const url = "https://course-api.com/react-tours-project";
 
 function App() {
+  const [tours, setTours] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const { data } = await axios.get(url);
+      setLoading(false);
+      setTours(data);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <>
+        <main>
+          <Loading />
+        </main>
+      </>
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main>
+        <Tours tours={tours} />
+      </main>
+    </>
   );
 }
 
---
+++ src/Tour.js
@@ -0,0 +1,17 @@
+const Tour = ({ id, name, info, image, price }) => {
+  return (
+    <div className="single-tour">
+      <img src={image} alt={name} />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price} </h4>
+        </div>
+        <p>{info} </p>
+        <button className="delete-btn">Not Interested</button>
+      </footer>
+    </div>
+  );
+};
+
+export default Tour;
